34d78b82413e7d7a3f963a6f634fa97649225797
Paul Kosel <my@procrastination.life>
2023-07-28 02:31:42 +0200

    App will now track when a token expires and automatically attempt to refresh it when necessary

--- src-tauri/src/oauth/mod.rs
+++ src-tauri/src/oauth/mod.rs
@@ -1,16 +1,23 @@
 use oauth2::basic::BasicClient;
-use oauth2::reqwest::http_client;
+use oauth2::reqwest::{async_http_client, http_client};
 use oauth2::{
-    AuthUrl, AuthorizationCode, ClientId, CsrfToken, PkceCodeChallenge, RedirectUrl, Scope,
-    TokenUrl,
+    AuthUrl, AuthorizationCode, ClientId, CsrfToken, PkceCodeChallenge, RedirectUrl, RefreshToken,
+    Scope, TokenResponse, TokenUrl,
 };
 use tauri::plugin::{Builder, TauriPlugin};
 use tauri::{command, Runtime, Window};
 use tauri_plugin_oauth::{start_with_config, OauthConfig};
 use url::Url;
 
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+struct RefreshResponse {
+    auth_token: String,
+    refresh_token: String,
+    lifetime: u128,
+}
+
 #[command]
-async fn do_oauth<R: Runtime>(app: tauri::AppHandle<R>, window: Window<R>) -> Result<u16, String> {
+async fn do_oauth<R: Runtime>(window: Window<R>) -> Result<u16, String> {
     let client = BasicClient::new(
         ClientId::new("loothound".to_string()),
         None,
@@ -65,8 +72,31 @@ async fn do_oauth<R: Runtime>(app: tauri::AppHandle<R>, window: Window<R>) -> Re
     .map_err(|err| err.to_string())
 }
 
+#[command]
+async fn attempt_refresh<R: Runtime>(window: Window<R>, refresh_token: String) {
+    let client = BasicClient::new(
+        ClientId::new("loothound".to_string()),
+        None,
+        AuthUrl::new("https://www.pathofexile.com/oauth/authorize".to_string()).unwrap(),
+        Some(TokenUrl::new("https://www.pathofexile.com/oauth/token".to_string()).unwrap()),
+    )
+    .set_redirect_uri(RedirectUrl::new("http://localhost:61360/auth".to_string()).unwrap());
+    let resp = client
+        .exchange_refresh_token(&RefreshToken::new(refresh_token))
+        .request_async(async_http_client)
+        .await;
+
+    match resp {
+        Ok(token) => {
+            println!("{:?}", token);
+            window.emit("oauth_token", token).unwrap();
+        }
+        Err(_) => (),
+    }
+}
+
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("oauth")
-        .invoke_handler(tauri::generate_handler![do_oauth])
+        .invoke_handler(tauri::generate_handler![do_oauth, attempt_refresh])
         .build()
 }
--- src/AuthContext.tsx
+++ src/AuthContext.tsx
@@ -1,35 +1,57 @@
-import { listen } from "@tauri-apps/api/event";
-import React, { createContext, useContext, useEffect, useState } from "react";
+import { invoke } from '@tauri-apps/api';
+import { listen } from '@tauri-apps/api/event';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
-const OAuthContext = createContext("");
+const OAuthContext = createContext('');
 
 interface Payload {
-  access_token: string;
+	access_token: string;
+	refresh_token: string;
+	expires_in: number;
+}
+
+async function refresh() {
+	const valid_until_str = localStorage.getItem('valid_until');
+	if (valid_until_str === null) return;
+	const valid_until = parseInt(valid_until_str);
+	if (Date.now() - valid_until >= 0) {
+		await invoke('plugin:oauth|attempt_refresh', {
+			refreshToken: localStorage.getItem('refresh_token'),
+		});
+		console.log('refreshing');
+	}
 }
 
 export function OAuthProvider({ children }: { children: React.ReactNode }) {
-  const [token, setToken] = useState("");
+	const [token, setToken] = useState('');
 
-  useEffect(() => {
-    setToken(localStorage.getItem("oauth_token") || "");
-  }, []);
+	useEffect(() => {
+		setToken(localStorage.getItem('oauth_token') || '');
+	}, []);
 
-  useEffect(() => {
-    const unlisten = listen("oauth_token", (evt) => {
-      const payload: Payload = evt.payload as Payload;
-      setToken(payload.access_token as string);
-      localStorage.setItem("oauth_token", payload.access_token as string);
-    });
-    return () => {
-      unlisten.then((f) => f());
-    };
-  }, [setToken]);
+	useEffect(() => {
+		const unlisten = listen('oauth_token', (evt) => {
+			const payload: Payload = evt.payload as Payload;
+			setToken(payload.access_token as string);
+			localStorage.setItem('oauth_token', payload.access_token);
+			localStorage.setItem('refresh_token', payload.refresh_token);
+			localStorage.setItem('valid_until', (Date.now() + payload.expires_in * 1000).toString());
+		});
+		return () => {
+			unlisten.then((f) => f());
+		};
+	}, [setToken]);
 
-  return (
-    <OAuthContext.Provider value={token}>{children}</OAuthContext.Provider>
-  );
+	useEffect(() => {
+		const id = setInterval(refresh, 60000 * 60);
+		refresh();
+
+		return () => clearInterval(id);
+	}, []);
+
+	return <OAuthContext.Provider value={token}>{children}</OAuthContext.Provider>;
 }
 
 export default function useAuth() {
-  return useContext(OAuthContext);
+	return useContext(OAuthContext);
 }
