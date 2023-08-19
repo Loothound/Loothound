5e252cf872be3a3f6c2fce31f73d7fee82d93b3c
Paul Kosel <my@procrastination.life>
2023-08-19 12:39:06 +0200

    fix migration, version bump

--- src-tauri/migrations/20230818182955_crucible_to_standard.sql
+++ src-tauri/migrations/20230818182955_crucible_to_standard.sql
@@ -1,4 +1,4 @@
 -- Add migration script here
 UPDATE stashes SET league = "Standard" WHERE league = "Crucbile";
-UPDATE profiles SET league_id = "Standard" WHERE league = "Crucible";
+UPDATE profiles SET league_id = "Standard" WHERE league_id = "Crucible";
 UPDATE profiles SET pricing_league = "Standard" WHERE pricing_league = "Crucible";
